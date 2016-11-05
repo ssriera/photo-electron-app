04fc8d5ceedc488705ade22b6e9786bc2f40ab56
Stephanie Riera <stephanie@frontside.io>
2016-11-05 16:11:35 -0500

    Initial commit

--- pages/home/index.js
+++ pages/home/index.js
@@ -15,10 +15,6 @@ import { title, html } from './index.md';
 
 class HomePage extends React.Component {
 
-  static propTypes = {
-    articles: PropTypes.array.isRequired,
-  };
-
   componentDidMount() {
     document.title = title;
   }
@@ -27,12 +23,7 @@ class HomePage extends React.Component {
     return (
       <Layout className={s.content}>
         <div dangerouslySetInnerHTML={{ __html: html }} />
-        <h4>Articles</h4>
-        <ul>
-          {this.props.articles.map((article, i) =>
-            <li key={i}><a href={article.url}>{article.title}</a> by {article.author}</li>
-          )}
-        </ul>
+        <h4>Well hello there!!</h4>
         <p>
           <br /><br />
         </p>
